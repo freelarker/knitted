e1aec18acf2fd857e80091b67bb8487ff3595f74
freelarker <freelarker@gmail.com>
2015-04-10 12:24:28 +0300

    Fight graphics update
    - gunfire particles (temporary)

--- HCClone_Unity/Assets/Project/Code/UnityScripts/Weapons/WeaponView.cs
+++ HCClone_Unity/Assets/Project/Code/UnityScripts/Weapons/WeaponView.cs
@@ -3,21 +3,43 @@ using UnityEngine;
 
 public class WeaponView : MonoBehaviour {
 	[SerializeField]
-	private Transform _particleParent;
+	private Transform _tracerParticleParent;
+	[SerializeField]
+	private Transform _gunfireParticleParent;
 
 	[SerializeField]
-	private TracerParticleController _tpc;
-	public TracerParticleController ParticleController {
-		get { return _tpc; }
-	}
+	private ParticleSystem _tracerParticlePrefab;
+	[SerializeField]
+	private ParticleSystem _gunfireParticlePrefab; 
 
 	[SerializeField]
 	private float _bulletParticleDelay = 0f;
 
+	private TracerParticleController _tpc;
+	public TracerParticleController ParticleController {
+		get { return _tpc; }
+	}
+	private ParticleSystem _gunfireParticleInstance;
+
 	private float _particleGunOffset = 0f;
 
+	public void Awake() {
+		GameObject particleTracer = GameObject.Instantiate(_tracerParticlePrefab.gameObject) as GameObject;
+		particleTracer.transform.SetParent(_tracerParticleParent);
+		particleTracer.transform.localPosition = Vector3.zero;
+		particleTracer.transform.localRotation = Quaternion.identity;
+		particleTracer.transform.localScale = Vector3.one;
+		_tpc = particleTracer.GetComponent<TracerParticleController>();
+
+		_gunfireParticleInstance = (GameObject.Instantiate(_gunfireParticlePrefab.gameObject) as GameObject).GetComponent<ParticleSystem>();
+		_gunfireParticleInstance.transform.SetParent(_gunfireParticleParent);
+		_gunfireParticleInstance.transform.localPosition = Vector3.zero;
+		_gunfireParticleInstance.transform.localRotation = Quaternion.identity;
+		_gunfireParticleInstance.transform.localScale = Vector3.one;
+	}
+
 	public void Setup(Transform particleParent) {
-		_particleGunOffset = particleParent.InverseTransformPoint(_particleParent.TransformPoint(_particleParent.localPosition)).x;
+		_particleGunOffset = particleParent.InverseTransformPoint(_tracerParticleParent.TransformPoint(_tracerParticleParent.localPosition)).x;
 		_tpc.transform.parent = particleParent;
 	}
 
@@ -29,6 +51,10 @@ public class WeaponView : MonoBehaviour {
 
 			_tpc.Particles.Simulate(_bulletParticleDelay, true);
 			_tpc.Particles.Play(true);
+
+			_gunfireParticleInstance.gameObject.SetActive(true);
+			_gunfireParticleInstance.Simulate(0f, true);
+			_gunfireParticleInstance.Play(true);
 		}
 	}
 

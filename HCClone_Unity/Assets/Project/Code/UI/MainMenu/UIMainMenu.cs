3b56834173659f86d5f5610f7ee823c4ccc71b88
freelarker <freelarker@gmail.com>
2015-04-22 10:49:57 +0300

    Main menu scene

---
+++ HCClone_Unity/Assets/Project/Code/UI/MainMenu/UIMainMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMainMenu : MonoBehaviour {
+	[SerializeField]
+	private Button _btnCampaign;
+	[SerializeField]
+	private Button _btnArena;
+	[SerializeField]
+	private Button _btnSets;
+	[SerializeField]
+	private Button _btnShop;
+	[SerializeField]
+	private Button _btnHeroes;
+	[SerializeField]
+	private Button _btnSquad;
+
+	public void Awake() {
+		float widthRatio = 1f * Screen.width / 1048;
+		float heightRatio = 1f * Screen.height / 540;
+
+		gameObject.GetComponent<CanvasScaler>().scaleFactor = Mathf.Min(widthRatio, heightRatio);
+	}
+
+	public void Start() {
+		_btnCampaign.onClick.AddListener(OnCampaignClick);
+	}
+
+	private void OnCampaignClick() {
+		//TODO: load last planet's scene
+		Application.LoadLevel("Planet1");
+	}
+}
